7d77892354b78f27d58855a6498d16289794db16
erwin.chen <erwin.chen@rci.rogers.com>
2021-12-21 15:50:59 -0500

    update directive for fields in schema

--- src/schema.js
+++ src/schema.js
@@ -63,10 +63,10 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+    "The module's length in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's full duration, in seconds"
-    durationInSeconds: Int @deprecated(reason: "Use durationInSeconds")
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
